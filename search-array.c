6ec887250f54c21d4451eddff01da3b4a497fb52
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2022-11-28 07:20:47 +0000

    gnkb dvhsfibuht

--- search-array.c
+++ search-array.c
@@ -2,11 +2,27 @@
 #include<stdio.h>
 int main(){
     
-    int i,a[];
- 
+    int i,a[0];
+ int c;
     for(i=0;i<5;i++){
         scanf("%d",&a[i]);
 
+    }
+
+
+printf("Number for search:- ");
+scanf("%d",&c);
+
+    for(i=0;i<5;i++){
+
+        if(c=a[i]){
+
+            printf("found");
+
+        }
+        else{
+            printf("Not Found");
+        }
     }
     
     return 0;
