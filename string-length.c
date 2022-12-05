b5a1cf68ada7ca04967616ac8c02de5039cc6717
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2022-12-05 07:14:13 +0000

    ok

---
+++ string-length.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+ 
+ int b;
+ char a[40];
+
+printf("Enter the value: ");
+gets(a);
+
+
+ 
+ 
+ b=strlen(a);
+        printf("The length of the string is %d \n",b);
+
+}
