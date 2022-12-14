65f12e681dda3561180b00855f04755fd8af7b32
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2022-12-14 06:41:22 +0000

    pointers

--- swap-functions.c
+++ swap-functions.c
@@ -15,6 +15,21 @@ int swap(int num1,int num2){
     printf("num2= %d \n",num2);
 
     }
+
+void swap1(int * num1, int * num2){
+
+    int temp;
+    printf("Entered Values %d %d",num1,num2);
+
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+    printf("After swapped values :- \n");
+    printf("num1= %d \n ",num1);
+    printf("num2= %d \n",num2);
+}
+
+
 int main(){
 
     int n1,n2;
@@ -22,7 +37,7 @@ int main(){
     printf("Enter two numbers : - ");
     scanf("%d%d",n1,n2);
 
-    swap(n1,n2);
+    swap1(n1,n2);
 
     return 0;
 }
