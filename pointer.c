d41c08b718a602adcc735cb8ba4799bba6a0a6c8
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2022-12-15 06:12:11 +0000

    pointer

---
+++ pointer.c
@@ -0,0 +1,14 @@
+/* NOTES:- 
+ pointer is a variable which is used to store address of another variable.
+function of * :- it is used to declare the pointer and it returns the value of the reference varible.
+function of & :- used to return the address of the varible.
+ types of pointer :- 
+ 1. null pointer:- will return the value 0. 
+ 2. void pointer:-  it is also termed as generic pointer,it does not have any standard data type. it can be used to store an address of any variable 
+ 3. wild pointer:- pointer is said to wild pointer if it is not being initiazled to anything. these types of poniters are not effecient because they may point to some  unknown memory location which may cause compilation error in our program and ulitmately crashing of the program. the result will be segementation error
+  */
+#include<stdio.h>
+int main(){
+
+    return 0;
+}
