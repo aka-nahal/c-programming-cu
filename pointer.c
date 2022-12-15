74adfd5e1b5493959beb0b1e02c6deddd0f792f2
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2022-12-15 06:28:42 +0000

    operator

--- pointer.c
+++ pointer.c
@@ -5,10 +5,67 @@ function of & :- used to return the address of the varible.
  types of pointer :- 
  1. null pointer:- will return the value 0. 
  2. void pointer:-  it is also termed as generic pointer,it does not have any standard data type. it can be used to store an address of any variable 
- 3. wild pointer:- pointer is said to wild pointer if it is not being initiazled to anything. these types of poniters are not effecient because they may point to some  unknown memory location which may cause compilation error in our program and ulitmately crashing of the program. the result will be segementation error
+ 3. wild pointer:- pointer is said to wild pointer if it is not being initiazled to anything. these types of poniters are not effecient because they may point to some  unknown memory location which may cause compilation error in our program and ulitmately crashing of the program. the result will be segementation error.
+ 
+ Pointer arithemetic operations:-
+  1. increment :-  new address = current address + (i*size of data type) syntax = p = p+1;
+  2. decrement :-  new address = current address - (i*size of data type) syntax = p = p-1;
+  3. addition :-   new address = current address - (number*size of data type)
+  4. subraction :- 
+
   */
 #include<stdio.h>
+int increment(){
+ int a=10;
+ int *p;
+ p = &a;
+
+ printf("%d \n",p);
+ p = p+1;
+ printf("%d \n",p);
+
+    return 0;
+}
+
+int decrement(){
+ int a=10;
+ int *p;
+ p = &a;
+
+ printf("%d \n",p);
+ p = p-1;
+ printf("%d \n",p);
+
+    return 0;
+}
+int addition(){
+ int a=10;
+ int *p;
+ p = &a;
+
+ printf("%d \n",p);
+ p = p+6;
+ printf("%d \n",p);
+
+    return 0;
+}
+int subraction(){
+ int a=10;
+ int *p;
+ p = &a;
+
+ printf("%d \n",p);
+ p = p-6;
+ printf("%d \n",p);
+
+    return 0;
+}
 int main(){
+    
+    increment();
+    decrement();
+    addition();
+    subraction();
 
     return 0;
 }
